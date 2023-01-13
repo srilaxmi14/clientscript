6f98b3402325ed24ab16a3dd30908594933cc914
Shetty <srilaxmi.shetty@wipfli.com>
2023-01-13 15:02:50 +0530

    update

--- src/FileCabinet/SuiteScripts/vtu_student_ue.js
+++ src/FileCabinet/SuiteScripts/vtu_student_ue.js
@@ -11,7 +11,7 @@ define(['N/record', 'N/search'],
         /**
          * Defines the function definition that is executed before record is loaded.
          * @param {Object} scriptContext
-         * @param {Record} scriptContext.newRecord - New record
+         * @param {Record} scriptContext.newRecord.id - New record
          * @param {string} scriptContext.type - Trigger type; use values from the context.UserEventType enum
          * @param {Form} scriptContext.form - Current form
          * @param {ServletRequest} scriptContext.request - HTTP request information sent from the browser for a client action only.
@@ -20,6 +20,7 @@ define(['N/record', 'N/search'],
         const beforeLoad = (scriptContext) => {
             log.debug("testing");
             var record=scriptContext.newRecord;
+
             var mode=scriptContext.type;
             if(mode=='view')
             {
@@ -31,12 +32,51 @@ define(['N/record', 'N/search'],
 
             }
 
+        }
+
+        const beforeSubmit = (scriptContext) => {
+            var record=scriptContext.newRecord;
+            var subjectone=record.getValue({
+                fieldId: 'custrecord1438'
+            });
+
+            var subjecttwo=record.getValue({
+                fieldId: 'custrecord1439'
+            })
+
+            var total=subjectone+subjecttwo;
+
+            record.setValue({
+                fieldId: 'custrecord1440',
+                value:total
             
+            })
+            
+            
+
+        }
+
+        const afterSubmit = (scriptContext) => {
+            var currecord=scriptContext.newRecord;
+            var recid=scriptContext.newRecord.id;
+            var name=currecord.getValue({
+                fieldId:'custrecord_wipfli_vtu_name'
+            })
+
+            record.submitFields({
+                type: 'customrecord_wipfli_student',
+                id: name,
+                values: {
+                    'custrecord_wipfli_student_vtu': recid
+                } 
+            })
         }
 
     
         return {
-            beforeLoad
+            beforeLoad,
+            beforeSubmit,
+            afterSubmit
         }
 
     });
